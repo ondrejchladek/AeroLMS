48d86787395d02aa9f351c6973ce4346135f4a3f
ondrejchladek <ondrejchladek@gmail.com>
2025-09-30 00:16:31 +0200

    Changes to be committed: 	modified:   src/app/api/users/[userId]/route.ts

--- src/app/api/users/[userId]/route.ts
+++ src/app/api/users/[userId]/route.ts
@@ -3,7 +3,7 @@ import { getServerSession } from 'next-auth';
 import { authOptions } from '@/lib/auth';
 import { prisma } from '@/lib/prisma';
 import { isAdmin } from '@/types/roles';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 
 interface Props {
   params: Promise<{
